a1895a17831ab3d068f93bce64ab06f9fa9de401
pufflik <pufflik@gmail.com>
2020-07-13 19:44:27 +0300

    Wrap popup children in portal

--- hooks/use-popup.tsx
+++ hooks/use-popup.tsx
@@ -1,9 +1,15 @@
-import {ReactElement, useCallback, useState} from 'react'
+import {createElement, ReactElement, useCallback, useState} from 'react'
+import usePortal from 'react-useportal'
 const useAction = (fn, args) => useCallback(() => fn(args), [fn, args])
 
 export function usePopup(initial = false) {
   const [isShown, setIsShown] = useState(initial)
-  const Popup = useCallback((props: {children: ReactElement}) => isShown ? props.children : null, [isShown])
+  const {Portal} = usePortal()
+
+  const Popup = useCallback(
+    (props: {children: ReactElement}) => isShown ? createElement(Portal, null, props.children) : null,
+    [Portal, isShown],
+  )
 
   return {
     hide  : useAction(setIsShown, false),
